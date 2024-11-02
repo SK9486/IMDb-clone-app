177f4086413825cdb53b9444e2cbf4c31acf3ed6
valli74appan@gmail.com <valli74appan@gmail.com>
2024-11-02 18:55:16 +0530

    about page

---
+++ src/app/about/page.js
@@ -0,0 +1,24 @@
+
+function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-5"
+    >
+        <h1 className="text-2xl font-medium text-red-500">About 🎬</h1>
+        <p className="">
+        <span className="font-semibold">Welcome to MyMovieDB 🙌 your one-stop destination for all things movies and TV shows! 🌟 Here, you'll find a world of entertainment at your fingertips, from blockbuster hits to hidden gems.</span>
+        What We Offer 📽️
+            <ol className="mt-4 mb-4 flex flex-col gap-2">
+                <li className="mt-4 mb-4" ><span className="font-semibold">Comprehensive Database 🎥 : </span>Discover detailed information on thousands of movies, TV shows, and the talented individuals who make them happen – directors, actors, writers, and more!</li>
+                <li className="mt-2 mb-2"><span className="font-semibold">Ratings & Reviews ⭐: </span>Share your opinions and read reviews from other movie lovers. Help others find the best content out there!</li>
+                <li className="mt-2 mb-2"><span className="font-semibold">Top Charts & Recommendations 🎉:</span> Stay updated on trending titles, top-rated films, and personalized recommendations just for you.</li>
+                <li className="mt-2 mb-2"><span className="font-semibold">Latest News 📰: </span>Dive into the latest happenings in the world of cinema and get exclusive insights on upcoming releases.</li>
+                <li className="mt-4 mb-4" ><span className="font-semibold">Our Mission 🌐 : </span>At MyMovieDB, our mission is to bring the joy of movies and TV to everyone, everywhere. We believe in connecting people through storytelling and making it easy for fans to explore, share, and celebrate the art of filmmaking.</li>
+                <li className="mt-2 mb-2"><span className="font-semibold">Join Our Community 🎭: </span>Whether you’re a casual viewer or a film buff, MyMovieDB is here for you. Create an account, join discussions, rate your favorite movies, and be part of a community that celebrates the magic of entertainment!</li>
+            </ol>
+            <h1 className="font-semibold">Happy watching! 🍿</h1>
+        </p>
+    </div>
+  )
+}
+
+export default page
